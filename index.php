bf04445b1554bfa3524d22957c0912d43cdd8eed
Steve Halford <steve.halford@basekit.com>
2012-03-09 21:03:14 +0000

    Added less.js and an index.php for the client app

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>JamJar</title>
+	<link rel="stylesheet/less" type="text/css" href="css/bootstrap/bootstrap.less">
+	<script type="text/javascript" charset="utf-8">less = {}; less.env = 'development';</script>
+	<script src="js/less-1.2.2.min.js" type="text/javascript"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+			<h1>JamJar</h1>
+		</div>
+	</div>
+
+	<script src="http://documentcloud.github.com/underscore/underscore-min.js"></script>
+	<script src="http://documentcloud.github.com/backbone/backbone-min.js"></script>   
+	<script src="js/app.js"></script>   
+</body>
+</html>
